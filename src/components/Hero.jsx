e683792c716a859ad779d009c74bbcf0dec93d60
Mobey-eth <chantler.aob@gmail.com>
2023-05-04 00:43:32 +0100

    Hero Build 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import GetStarted from "./GetStarted"
 
 const Hero = () => (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className= {`flex-1 ${styles.flexStart} flex-col xl:px-0 ${styles.paddingX}`}>
+      <div className= {`flex-1 ${styles.flexStart} flex-col xl:px-2 ${styles.paddingX}`}>
         <div className="flex flex-row items-center py-[6px] px-[4] bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]"/>
           <p className={`${styles.paragraph} ml-2`}>
@@ -24,14 +24,34 @@ const Hero = () => (
             The Next <br className="sm:block hidden" /> 
             {" "}
             <span className="text-gradient">Generation</span> {" "}
-            Payment Method. 
           </h1>
 
           <div className="sm:flex hidden md:mr-4 mr-0 text-white">
             <GetStarted />
           </div>
+        </div>
+
+        <h1 className="font-poppins text-white font-semibold ss:text-[68px] text-[52px] ss:leading-[100px] leading-[75px] w-full" >
+          Payment Method.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. 
+          We examine annual percentage rates, annual fees.
+        </p>
 
         </div>
+          <img src={robot} alt="billing" className="w-[100%] h-[100%] sm:w-[50%] sm:h-[60%] relative z-[5] mt-2" />
+          <div className="absolute w-[40%] h-[35%] top-0 pink__gradient z-[0]" />
+          <div className="absolute w-[80%] h-[80%] bottom-40 rounded-full white__gradient z-[1]" />
+          <div className="absolute w-[50%] h-[50%] bottom-20 right-20 blue__gradient z-[0]" />
+        <div>
+
+      </div>
+      <div className={`${styles.flexCenter}`}>
+        <div className="sm:hidden">
+        <GetStarted />
+        </div>
+        
       </div>
     </section>
   )
