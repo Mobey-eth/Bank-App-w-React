24dab3ca4714406891f8cc4c5ad4f7b7b6335c53
Mobey-eth <chantler.aob@gmail.com>
2023-05-08 23:25:53 +0100

    Hero Build 4- contd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => (
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. 
-          We examine annual percentage rates, annual fees.
+          We examine annual percentage rates, annual fees and other statistical data.
         </p>
 
         </div>
